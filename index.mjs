9272cdc2d62c83f566d917bdcbfc9068374c85c3
Giovane J. O. Freitas <gjofreitas@pbh.gov.br>
2023-07-10 20:38:34 -0300

    Criand backend

---
+++ index.mjs
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import "./loadEnvironment.mjs";
+import "express-async-errors";
+import cadastros from "./routes/cadastros.mjs";
+
+const PORT = process.env.PORT || 5050;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Load the /cadastros routes
+app.use("/cadastros", cadastros);
+
+// Global error handling
+app.use((err, _req, res, next) => {
+  console.log(err);
+  res.status(500).send("Uh oh! An unexpected error occured.")
+})
+
+// start the Express server
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
