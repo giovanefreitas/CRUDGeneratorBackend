10ba82ab4140f662be9914015d6376862c0658c6
Giovane J. O. Freitas <gjofreitas@pbh.gov.br>
2023-08-01 17:34:35 -0300

    Criando importador de BD

--- index.mjs
+++ index.mjs
@@ -4,6 +4,7 @@ import "./loadEnvironment.mjs";
 import "express-async-errors";
 import cadastros from "./routes/cadastros.mjs";
 import exportacao from "./routes/exportacao.mjs";
+import importacao from "./routes/importacao.mjs";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
@@ -14,11 +15,12 @@ app.use(express.json());
 // Load the /cadastros routes
 app.use("/cadastros", cadastros);
 app.use("/exportar", exportacao);
+app.use("/importar", importacao);
 
 // Global error handling
 app.use((err, _req, res, next) => {
   console.log(err);
-  res.status(500).send("Uh oh! An unexpected error occured.");
+  res.status(500).send("Ocorreu um erro inesperado.");
 });
 
 // start the Express server
---
+++ routes/importacao.mjs
@@ -0,0 +1,106 @@
+import express from "express";
+import oracledb from "oracledb";
+
+const router = express.Router();
+
+oracledb.initOracleClient({ libDir: process.env.INSTANT_CLIENT_HOME });
+oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
+
+// Get a list of 50 cadastros
+router.post("/oracle", async (req, res) => {
+  console.log(req.body.host + ":" + req.body.port + "/" + req.body.serviceName);
+
+  const connection = await oracledb.getConnection({
+    user: req.body.user,
+    password: req.body.password,
+    connectString:
+      req.body.host + ":" + req.body.port + "/" + req.body.serviceName,
+  });
+
+  try {
+    let tables = await findTables(connection, req.body.owner);
+    let project = { name: req.body.owner };
+    project.screens = [];
+
+    for (let table of tables) {
+      project.screens.push(
+        await generateScreen(connection, req.body.owner, table.TABLE_NAME)
+      );
+    }
+
+    res.send(project).status(200);
+  } finally {
+    await connection.close();
+  }
+});
+
+async function findTables(connection, owner) {
+  const result = await connection.execute(
+    `SELECT TABLE_NAME FROM all_tables WHERE OWNER = :owner`,
+    { owner }
+  );
+
+  return result.rows;
+}
+
+async function generateScreen(connection, owner, table) {
+  let screen = {
+    label: "Cadastro de " + normalizeText(table),
+    entity: toCamelCase(table),
+    name: singularIdentifier(table),
+    plural_name: pluralIdentifier(table),
+    type: "grid",
+    subfields: await generateFields(connection, owner, table),
+  };
+
+  return screen;
+}
+
+async function generateFields(connection, owner, tableName) {
+  const result = await connection.execute(
+    `SELECT column_name, data_type, data_length, data_precision, nullable 
+    FROM all_tab_columns WHERE owner = :owner AND table_name = :tableName`,
+    { owner, tableName }
+  );
+
+  let fields = [];
+
+  for (let column of result.rows) {
+    fields.push({
+      id: column.COLUMN_NAME,
+      name: column.COLUMN_NAME,
+      label: column.COLUMN_NAME,
+      type: translateType(column.DATA_TYPE),
+      subfields: [],
+    });
+  }
+
+  return fields;
+}
+
+function translateType(type) {
+  switch (type) {
+    case "VARCHAR2":
+      return "input";
+    default:
+      return "input";
+  }
+}
+
+function normalizeText(text) {
+  return text;
+}
+
+function toCamelCase(text) {
+  return text;
+}
+
+function singularIdentifier(text) {
+  return text;
+}
+
+function pluralIdentifier(text) {
+  return text;
+}
+
+export default router;
