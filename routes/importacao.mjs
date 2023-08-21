edad495973e5793f4b7cca9a1a2f965a1baadb4d
Giovane J. O. Freitas <gjofreitas@pbh.gov.br>
2023-08-21 17:43:34 -0300

    Implementando suporte a relacionamentos

--- routes/importacao.mjs
+++ routes/importacao.mjs
@@ -212,6 +212,7 @@ async function generateRelationships(
         referencedOwner: column.R_OWNER,
         referencedTable: column.R_TABLE,
         constraintName: column.R_CONSTRAINT_NAME,
+        referencedEntity: column.R_TABLE,
       }
     );
 
